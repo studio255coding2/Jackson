0c342e98f167c02a6b28a4296910f6529dad2ffc
studio255coding2 <123024166+studio255coding2@users.noreply.github.com>
2023-01-25 21:50:24 +0000

    week 2

--- week 0 (What they start with)/script.js
+++ week 0 (What they start with)/script.js
@@ -1,12 +1,17 @@
 var scene = new THREE.Scene()
 
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-
+camera.position.z = 5
 var renderer = new THREE.WebGLRenderer({antialias: true})
 renderer.setClearColor("lightblue")
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
+var geometry = new THREE.BoxGeometry(1, 1, 1)
+var material = new THREE.MeshLambertMaterial({color: "blue"})
+var cube1 = new THREE.Mesh(geometry, material)
+scene.add(cube1)
 
+cube1.rotation.y = 45 
 renderer.render(scene, camera)
